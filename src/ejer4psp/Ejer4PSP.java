384d9be78e75b8090d80e7893c73102dc3a953ae
Nabor <Nabor@DESKTOP-BTR17DU.home>
2016-11-05 19:38:55 +0100

    Fin

---
+++ src/ejer4psp/Ejer4PSP.java
@@ -0,0 +1,19 @@
+
+package ejer4psp;
+
+/**
+ *
+ * @author Nabor
+ */
+public class Ejer4PSP {
+
+   
+    public static void main(String[] args) {
+        new SumNumPares("Suma de pares ").start();
+        new SumNumImpares("Suma de impares ").start();
+        new SumDosTres("Suma de numeros acabados en dos y tres ").start();
+          
+        
+    }
+    
+}
